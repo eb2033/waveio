29e1c281c17f9c661d840814ddbf10d72925349e
eb2033 <egor.bodrov@gmail.com>
2025-11-02 15:00:32 +0000

    Update encoding.py

--- encoding.py
+++ encoding.py
@@ -5,3 +5,15 @@ class PCMEncoding(IntEnum):
     SIGNED_16 = 2
     SIGNED_24 = 3
     SIGNED_32 = 4
+
+@property
+    def max(self):
+        return 255 if self == 1 else -self.min - 1
+
+    @property
+    def min(self):
+        return 0 if self == 1 else -(2 ** (self.num_bits - 1))
+
+    @property
+    def num_bits(self):
+        return 8 * self
